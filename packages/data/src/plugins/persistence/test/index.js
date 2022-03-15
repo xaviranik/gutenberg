54bbce82693e0591d4a3e263c9cc1fcc4b25e83a
Daniel Richards <daniel.richards@automattic.com>
2022-03-15 14:39:54 +0800

    Migrate local storage data from interface to preferences

--- packages/data/src/plugins/persistence/test/index.js
+++ packages/data/src/plugins/persistence/test/index.js
@@ -12,6 +12,7 @@ import plugin, {
 	migrateFeaturePreferencesToPreferencesStore,
 	migrateThirdPartyFeaturePreferencesToPreferencesStore,
 	migrateIndividualPreferenceToPreferencesStore,
+	migrateInterfaceEnableItemsToPreferencesStore,
 } from '../';
 import objectStorage from '../storage/object';
 import { createRegistry } from '../../../';
@@ -919,6 +920,125 @@ describe( 'migrateThirdPartyFeaturePreferencesToPreferencesStore', () => {
 						},
 					},
 				},
+			},
+		} );
+	} );
+} );
+
+describe( 'migrateInterfaceEnableItemsToPreferencesStore', () => {
+	it( 'migrates enableItems to the preferences store', () => {
+		const persistenceInterface = createPersistenceInterface( {
+			storageKey: 'test-username',
+		} );
+
+		persistenceInterface.set( 'core/interface', {
+			enableItems: {
+				singleEnableItems: {
+					complementaryArea: {
+						'core/edit-post': 'edit-post/document',
+						'core/edit-site': 'edit-site/global-styles',
+					},
+				},
+				multipleEnableItems: {
+					pinnedItems: {
+						'core/edit-post': {
+							'plugin-1': true,
+						},
+						'core/edit-site': {
+							'plugin-2': true,
+						},
+					},
+				},
+			},
+		} );
+
+		migrateInterfaceEnableItemsToPreferencesStore( persistenceInterface );
+
+		expect( persistenceInterface.get() ).toEqual( {
+			'core/preferences': {
+				preferences: {
+					'core/edit-post': {
+						complementaryArea: 'edit-post/document',
+						pinnedItems: {
+							'plugin-1': true,
+						},
+					},
+					'core/edit-site': {
+						complementaryArea: 'edit-site/global-styles',
+						pinnedItems: {
+							'plugin-2': true,
+						},
+					},
+				},
+			},
+			'core/interface': {
+				enableItems: undefined,
+			},
+		} );
+	} );
+
+	it( 'merges pinnedItems and complementaryAreas with existing preferences store data', () => {
+		const persistenceInterface = createPersistenceInterface( {
+			storageKey: 'test-username',
+		} );
+
+		persistenceInterface.set( 'core/interface', {
+			enableItems: {
+				singleEnableItems: {
+					complementaryArea: {
+						'core/edit-post': 'edit-post/document',
+						'core/edit-site': 'edit-site/global-styles',
+					},
+				},
+				multipleEnableItems: {
+					pinnedItems: {
+						'core/edit-post': {
+							'plugin-1': true,
+						},
+						'core/edit-site': {
+							'plugin-2': true,
+						},
+					},
+				},
+			},
+		} );
+
+		persistenceInterface.set( 'core/preferences', {
+			preferences: {
+				'core/edit-post': {
+					preferenceA: 1,
+					preferenceB: 2,
+				},
+				'core/edit-site': {
+					preferenceC: true,
+				},
+			},
+		} );
+
+		migrateInterfaceEnableItemsToPreferencesStore( persistenceInterface );
+
+		expect( persistenceInterface.get() ).toEqual( {
+			'core/preferences': {
+				preferences: {
+					'core/edit-post': {
+						preferenceA: 1,
+						preferenceB: 2,
+						complementaryArea: 'edit-post/document',
+						pinnedItems: {
+							'plugin-1': true,
+						},
+					},
+					'core/edit-site': {
+						preferenceC: true,
+						complementaryArea: 'edit-site/global-styles',
+						pinnedItems: {
+							'plugin-2': true,
+						},
+					},
+				},
+			},
+			'core/interface': {
+				enableItems: undefined,
 			},
 		} );
 	} );
