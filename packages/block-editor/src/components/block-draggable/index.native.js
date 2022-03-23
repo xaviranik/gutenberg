a3626141bce6c77186e8fd51c55de236d8e938e7
Carlos Garcia <fluiddot@gmail.com>
2022-03-23 19:37:57 +0100

    Introduce useScrollWhenDragging hook

--- packages/block-editor/src/components/block-draggable/index.native.js
+++ packages/block-editor/src/components/block-draggable/index.native.js
@@ -24,6 +24,7 @@ import { useEffect } from '@wordpress/element';
 /**
  * Internal dependencies
  */
+import useScrollWhenDragging from './use-scroll-when-dragging';
 import DraggableChip from './draggable-chip';
 import { store as blockEditorStore } from '../../store';
 import { useBlockListContext } from '../block-list/block-list-context';
@@ -64,10 +65,19 @@ const BlockDraggableWrapper = ( { children } ) => {
 	const isDragging = useSharedValue( false );
 	const scrollAnimation = useSharedValue( 0 );
 
+	const [
+		startScrolling,
+		scrollOnDragOver,
+		stopScrolling,
+		draggingScrollHandler,
+	] = useScrollWhenDragging();
+
 	const scrollHandler = ( event ) => {
 		'worklet';
 		const { contentOffset } = event;
 		scroll.offsetY.value = contentOffset.y;
+
+		draggingScrollHandler( event );
 	};
 
 	// Stop dragging blocks if the block draggable is unmounted.
@@ -100,9 +110,13 @@ const BlockDraggableWrapper = ( { children } ) => {
 						( scroll.offsetY.value - blockLayout.y ) -
 						EXTRA_OFFSET_WHEN_CLOSE_TO_TOP_EDGE
 				);
-				scrollAnimation.value = withTiming( scrollOffsetTarget, {
-					duration: SCROLL_ANIMATION_DURATION,
-				} );
+				scrollAnimation.value = withTiming(
+					scrollOffsetTarget,
+					{ duration: SCROLL_ANIMATION_DURATION },
+					() => startScrolling( position.y )
+				);
+			} else {
+				runOnUI( startScrolling )( position.y );
 			}
 		}
 	};
@@ -150,6 +164,9 @@ const BlockDraggableWrapper = ( { children } ) => {
 
 		chip.x.value = dragPosition.x;
 		chip.y.value = dragPosition.y;
+
+		// Update scrolling velocity
+		scrollOnDragOver( dragPosition.y );
 	};
 
 	const stopDragging = () => {
@@ -158,6 +175,7 @@ const BlockDraggableWrapper = ( { children } ) => {
 
 		chip.scale.value = withTiming( 0 );
 		runOnJS( stopDraggingBlocks )();
+		stopScrolling();
 	};
 
 	const chipStyles = useAnimatedStyle( () => {
